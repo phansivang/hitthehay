1b390096806dd20fdcec6e22e6da69052ee3b6c2
phansivang <chengphansivang@peakwealthbank.com>
2025-11-06 23:20:37 +0700

    fix: github action

--- src/features/tasks/TaskDetail.tsx
+++ src/features/tasks/TaskDetail.tsx
@@ -1,4 +1,4 @@
-import React, { useMemo, useState } from 'react';
+import React, {useEffect, useMemo, useState} from 'react';
 import { useParams, Link } from 'react-router-dom';
 import { PlayCircle } from 'lucide-react';
 
